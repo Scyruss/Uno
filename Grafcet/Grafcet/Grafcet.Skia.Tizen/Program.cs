2f62897755244de74883bff3110bf91eb468cf62
Scyruss <64830174+Scyruss@users.noreply.github.com>
2021-12-05 18:29:27 +0100

    cxwcwx

---
+++ Grafcet/Grafcet/Grafcet.Skia.Tizen/Program.cs
@@ -0,0 +1,14 @@
+﻿using Tizen.Applications;
+using Uno.UI.Runtime.Skia;
+
+namespace Grafcet.Skia.Tizen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new TizenHost(() => new Grafcet.App(), args);
+            host.Run();
+        }
+    }
+}
---
+++ Grafcet/Grafcet/Grafcet.Skia.Wpf/Program.cs
@@ -0,0 +1,3 @@
+﻿namespace Grafcet.Skia.Gtk
+{
+}
